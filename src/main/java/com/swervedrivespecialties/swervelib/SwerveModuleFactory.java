bb9af5d88d31115021a7a5f54e5a829ffd0cb841
41robocats98 <60160962+41robocats98@users.noreply.github.com>
2023-02-02 19:10:46 -0600

    changed math
    fdsosizhfei[spa

--- src/main/java/com/swervedrivespecialties/swervelib/SwerveModuleFactory.java
+++ src/main/java/com/swervedrivespecialties/swervelib/SwerveModuleFactory.java
@@ -8,8 +8,8 @@ public class SwerveModuleFactory<DriveConfiguration, SteerConfiguration> {
     private final SteerControllerFactory<?, SteerConfiguration> steerControllerFactory;
 
     public SwerveModuleFactory(ModuleConfiguration moduleConfiguration,
-                               DriveControllerFactory<?, DriveConfiguration> driveControllerFactory,
-                               SteerControllerFactory<?, SteerConfiguration> steerControllerFactory) {
+            DriveControllerFactory<?, DriveConfiguration> driveControllerFactory,
+            SteerControllerFactory<?, SteerConfiguration> steerControllerFactory) {
         this.moduleConfiguration = moduleConfiguration;
         this.driveControllerFactory = driveControllerFactory;
         this.steerControllerFactory = steerControllerFactory;
@@ -22,17 +22,16 @@ public class SwerveModuleFactory<DriveConfiguration, SteerConfiguration> {
         return new ModuleImplementation(driveController, steerController);
     }
 
-    public SwerveModule create(ShuffleboardLayout container, DriveConfiguration driveConfiguration, SteerConfiguration steerConfiguration) {
+    public SwerveModule create(ShuffleboardLayout container, DriveConfiguration driveConfiguration,
+            SteerConfiguration steerConfiguration) {
         var driveController = driveControllerFactory.create(
                 container,
                 driveConfiguration,
-                moduleConfiguration
-        );
+                moduleConfiguration);
         var steerContainer = steerControllerFactory.create(
                 container,
                 steerConfiguration,
-                moduleConfiguration
-        );
+                moduleConfiguration);
 
         return new ModuleImplementation(driveController, steerContainer);
     }
@@ -57,43 +56,48 @@ public class SwerveModuleFactory<DriveConfiguration, SteerConfiguration> {
         }
 
         @Override
-        public double getDrivePosition(){
+        public double getDrivePosition() {
             return driveController.getStatePosition();
         }
 
         @Override
-        public void zeroDrivePosition(){
+        public void zeroDrivePosition() {
             driveController.resetStateDrivePosition();
         }
 
         @Override
         public void set(double driveVoltage, double wantedAngleRad) {
             // wantedAngleRad %= (2.0 * Math.PI); //TODO changed
-            if (wantedAngleRad < 0.0) {
-                wantedAngleRad += 2.0 * Math.PI;
-            }
+            // if (wantedAngleRad < 0.0) {
+            // wantedAngleRad += 2.0 * Math.PI;
+            // }
 
             double difference = wantedAngleRad - getSteerAngle();
-            // Change the target angle so the difference is in the range [-pi, pi) instead of [0, 2pi)
-            if (difference >= Math.PI) {
-                wantedAngleRad -= 2.0 * Math.PI;
-            } else if (difference < -Math.PI) {
-                wantedAngleRad += 2.0 * Math.PI;
-            }
-            difference = wantedAngleRad - getSteerAngle(); // Recalculate difference
+            // Change the target angle so the difference is in the range [-pi, pi) instead
+            // of [0, 2pi)
+            // if (difference >= Math.PI) {
+            // wantedAngleRad -= 2.0 * Math.PI;
+            // } else if (difference < -Math.PI) {
+            // wantedAngleRad += 2.0 * Math.PI;
+            // }
+            // difference = wantedAngleRad - getSteerAngle(); // Recalculate difference
 
-            // If the difference is greater than 90 deg or less than -90 deg the drive can be inverted so the total
+            // If the difference is greater than 90 deg or less than -90 deg the drive can
+            // be inverted so the total
             // movement of the module is less than 90 deg
             if (difference > Math.PI / 2.0 || difference < -Math.PI / 2.0) {
-                // Only need to add 180 deg here because the target angle will be put back into the range [0, 2pi)
+                // Only need to add 180 deg here because the target angle will be put back into
+                // the range [0, 2pi)
                 wantedAngleRad += Math.PI;
                 driveVoltage *= -1.0;
             }
 
             // Put the target angle back into the range [0, 2pi)
-            wantedAngleRad %= (2.0 * Math.PI);
+            // wantedAngleRad %= (2.0 * Math.PI);
             if (wantedAngleRad < 0.0) {
                 wantedAngleRad += 2.0 * Math.PI;
+            } else if (wantedAngleRad > 2.0 * Math.PI) {
+                wantedAngleRad -= 2.0 * Math.PI;
             }
 
             driveController.setReferenceVoltage(driveVoltage);
